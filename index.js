7d9e6348db93cef6d768abb1c56ca8e09a922ed5
George Wauchope <69352286+GeorgeWauchope@users.noreply.github.com>
2021-01-25 19:21:14 -0500

    js stuff

---
+++ index.js
@@ -0,0 +1,8 @@
+const franc = require('franc');
+const langs = require('langs');
+const input = process.argv[2];
+const langsCode = franc(input);
+
+const language = langs.where('3', langsCode);
+
+console.log(language.name);
